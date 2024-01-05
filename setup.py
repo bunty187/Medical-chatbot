d334a079b3c9996a8c9934222f9b43ee40053e92
bunty187 <buntyk3623@gmail.com>
2024-01-05 18:40:48 +0530

    final submission

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Bunty Kumar',
+    author_email= 'buntyk3623@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
